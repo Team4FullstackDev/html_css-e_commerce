3bcdaaec804f7687285f8056004827a4e9e26b02
nevgard <fahmifaza6@gmail.com>
2023-10-11 18:15:13 +0700

    first commit section 7,8,9

---
+++ assets/js/section7.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".container-card").slick({
+    arrows: false,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    variableWidth: true,
+  });
+});
