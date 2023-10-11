14304f102d4e095ea35cde000e058ab6a4574ec6
Nextiar3 <endrinastiar03@gmail.com>
2023-10-11 13:58:44 +0700

    fix/section10

--- assets/js/section10.js
+++ assets/js/section10.js
@@ -2,16 +2,16 @@ const filterButtons = document.querySelectorAll(".section__10_filter-button butt
 const filterableCards = document.querySelectorAll(".section__10_filterable-card .section__10_card");
 
 const filterCards = e => {
-    document.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
+    document.querySelector(".section__10_active").classList.remove("section__10_active");
+    e.target.classList.add("section__10_active");
 
 
     filterableCards.forEach(card => {
 
-        card.classList.add("hide");
+        card.classList.add("section__10_hide");
 
         if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === "section__10_all"){
-            card.classList.remove("hide");
+            card.classList.remove("section__10_hide");
         }
 
     })
