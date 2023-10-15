15eea73e17eecd6a4e5fa39fe3c5f71dfff8d30a
EffenrilAgung <effenrilagung300@gmail.com>
2023-10-15 19:03:45 +0700

    add section 6 Effenril

---
+++ assets/js/section6.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$('.section_6_lazyload_wrapper').slick({
+		arrows: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		infinite: true,
+		dosts: false,
+	});
+});
