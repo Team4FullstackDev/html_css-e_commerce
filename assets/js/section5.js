426a8460a28f73afe3a5a4256314feff5a4439a9
Affu321 <affudina663@gmail.com>
2023-10-17 14:13:23 +0800

    adding section4_section5_footer

---
+++ assets/js/section5.js
@@ -0,0 +1,11 @@
+// createdby affu satouru
+$(document).ready(function () {
+    $(".section__5-container-card").slick({
+      arrows: false,
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 6,
+      variableWidth: true,
+    });
+  });
+  
