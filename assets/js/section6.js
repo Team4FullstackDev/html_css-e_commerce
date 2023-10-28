2709f463cb5f107c63917b8ac98ba6fd93dc86b8
EffenrilAgung <effenrilagung300@gmail.com>
2023-10-29 00:36:38 +0700

    fixing bug mobile slide on phone

--- assets/js/section6.js
+++ assets/js/section6.js
@@ -1,11 +1,26 @@
-$(document).ready(function () {
-	$('.section_6_lazyload_wrapper').slick({
-		arrows: false,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1000,
-		infinite: true,
-		dosts: false,
-	});
-});
+function initializeSlider() {
+	if (window.matchMedia('(max-width: 425px)').matches) {
+		$('.section_6_lazyload_wrapper').slick({
+			arrows: false,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 1000,
+			infinite: true,
+			dosts: false,
+		});
+	} else {
+		$('.section_6_lazyload_wrapper').slick({
+			arrows: false,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 1000,
+			infinite: true,
+			dosts: false,
+		});
+	}
+}
+
+initializeSlider();
+window.addEventListener('resize', initializeSlider);
