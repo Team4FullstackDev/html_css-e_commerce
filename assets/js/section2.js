f67ba9ed3436c544f07aa0a60b113130a908faeb
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2023-10-28 22:29:37 +0700

    adjusting respnsive on section 2 and 3

--- assets/js/section2.js
+++ assets/js/section2.js
@@ -18,12 +18,3 @@ $(".section__2-button-prev").on("click", function () {
 $(".section__2-button-next").on("click", function () {
   $(".section__2-carousel").slick("slickNext");
 });
-
-$(document).ready(function () {
-  $(".section__2-list-store").slick({
-    inifinite: true,
-    autoplay: false,
-    slidesToShow: 6,
-    slidesToScroll: 6,
-  });
-});
